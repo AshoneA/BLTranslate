0bacfbf4ed06dfc9f27459f21ca545228c485bcb
Cygra <sjtuwbh@gmail.com>
2020-04-15 18:13:37 +0800

    调整结构

--- src/extension.ts
+++ src/extension.ts
@@ -209,10 +209,10 @@ export function activate(context: vscode.ExtensionContext) {
       if (pkg.name === "gc_native") {
         const appTranslateKey = handleAppFormat(text);
         const matchs = text.match(/(\${[^\${}]*})/g);
-        let lastStr = "";
         if (matchs) {
-          lastStr = `{${matchs.map((v) => v.slice(2, -1)).join(", ")}}`;
-          replaceText = `i18nConfigGlobal.w('${appTranslateKey}', ${lastStr})`;
+          replaceText = `i18nConfigGlobal.w('${appTranslateKey}', {${matchs
+            .map((v) => v.slice(2, -1))
+            .join(", ")}})`;
         } else {
           replaceText = `i18nConfigGlobal.t('${appTranslateKey}')`;
         }
