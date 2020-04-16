56e1308ee062d856030a97c854eade51e53c1a16
baozefeng <zefengbao@outlook.com>
2020-04-16 10:39:47 +0800

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.json']
   },
   module: {
     rules: [
